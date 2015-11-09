d820376b2cb17c06f893eeabf295a314e7f70600
Meiya Chang <hc6uj@virginia.edu>
2015-11-09 11:42:24 -0500

    updated hs.py

--- code/python/hs.py
+++ code/python/hs.py
@@ -1,15 +1,13 @@
 def hs(n):
     x = 0
     while (n != 1):
+        x = x + 1
         print(n)
         if (n % 2 == 0):
             n = n//2
-            x = x + 1
         else:
             n = 3 * n + 1
-            x = x + 1
-    while (n == 1):
-        x = x + 1
-        print(n)
-        print(x)
-        n = n - 1
+    print (n)
+    x = x + 1
+    print(x)
+    return None
